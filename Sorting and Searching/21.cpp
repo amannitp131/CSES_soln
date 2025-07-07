7cdf3201fa4ec2d52ae49e667a60c0fa9937a941
aman <amanm.ug23.cs@nitp.ac.in>
2025-07-07 18:36:57 +0530

    Done

--- Sorting and Searching/21.cpp
+++ Sorting and Searching/21.cpp
@@ -15,24 +15,43 @@ const int MOD = 1e9 + 7;
 void solve() {
     int n;
     cin >> n;
-    vector<int> vec(n);
-    iota(vec.begin(), vec.end(), 1);
-    int idx=1,k=1;
-    while(!vec.empty()){
-        cout<<vec[idx]<<" ";
-        vec.erase(vec.begin() + idx);
+    list<int> lst;
+    for (int i = 1; i <= n; i++) lst.push_back(i);
 
-        if (vec.empty()) break;
+    auto it = lst.begin();
+    while (!lst.empty()) {
+        // Move iterator one step forward (circularly)
+        if (it == lst.end()) it = lst.begin();
+        it++;
+        if (it == lst.end()) it = lst.begin();
 
-        if(idx+k+1>=n){
-            k++;
-            idx=idx+k-n;
-        }else{
-            idx=idx+k;
-        }
-    }  
+        cout << *it << " ";
+        it = lst.erase(it);
+        if (lst.empty()) break;
+    }
+    cout << endl;
 }
 
+// Circular Removal Simulation
+// You're simulating a circular removal of elements:
+// Start with 1 to n
+// Skip one element, remove the next
+// Continue until the list is empty
+// Output the order of removals
+//  using list and an iterator to manage circular traversal
+//   and efficient deletion (O(1) in lists).
+
+// why we use list here
+// In list, deleting an element takes constant time O(1)
+// In vector, deletion would be O(n) 
+// because all elements after the deleted one need to be shifted
+
+
+// Similar leetcode question
+// https://leetcode.com/problems/find-the-winner-of-the-circular-game/description/
+// https://leetcode.com/problems/combination-sum-iii/description/
+// https://leetcode.com/problems/reorder-list/description/
+// https://leetcode.com/problems/insertion-sort-list/description/
 
 
 
